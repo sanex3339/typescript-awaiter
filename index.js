20f2b9752299d6725ebc364c795716c4e908e652
kachalov <t.kachalov@advmaker.net>
2016-04-15 11:26:46 +0300

    Update awaiter to latest version

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-function __awaiter(thisArg, _arguments, Promise, generator) {
-    return new Promise(function (resolve, reject) {
-        generator = generator.call(thisArg, _arguments);
-        function cast(value) { return value instanceof Promise && value.constructor === Promise ? value : new Promise(function (resolve) { resolve(value); }); }
-        function onfulfill(value) { try { step("next", value); } catch (e) { reject(e); } }
-        function onreject(value) { try { step("throw", value); } catch (e) { reject(e); } }
-        function step(verb, value) {
-            var result = generator[verb](value);
-            result.done ? resolve(result.value) : cast(result.value).then(onfulfill, onreject);
-        }
-        step("next", void 0);
+function __awaiter(thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator.throw(value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected);
+    }
+        step((generator = generator.apply(thisArg, _arguments)).next());
     });
-}
+};
 
 if (typeof module !== 'undefined' && module.exports) {
     exports = module.exports = __awaiter;
